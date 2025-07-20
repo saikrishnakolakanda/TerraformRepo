4da38d2321c9daf1caab461191e7492e41217d23
SaiKrishna <ksai3980@gmail.com>
2025-07-20 15:55:32 +0530

    Day-4 is added

---
+++ Day4-statefile/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  
+}
+
+
+
+
+
+resource "aws_instance" "name" {
+  ami = "ami-05ffe3c48a9991133"
+  instance_type = "t2.micro"
+  key_name = "key1"
+  tags = {
+    Name = "day-4"
+  }
+}
