ff46c32e2513555cf1c6f1699b480a3420e515b7
root <root@ip-172-31-81-193.ec2.internal>
2025-07-21 16:23:01 +0000

    main.tf 4.1 modified in Ec2

--- Day5-statefilebackend/main.tf
+++ Day5-statefilebackend/main.tf
@@ -3,13 +3,8 @@ resource "aws_instance" "name" {
   instance_type = "t2.micro"
   key_name = "key1"
   tags = {
-    Name = "day-4"
+    Name = "day-4.1"
   }
-}
-
-resource "aws_s3_bucket" "name" {
-    bucket = "teraformhkhtecre"
-  
 }
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
