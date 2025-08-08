a6d88c25e2fa9d0641688446cabd9ef169ece3a6
SaiKrishna <ksai3980@gmail.com>
2025-08-08 17:59:30 +0530

    commiting changes

--- Day13-Null-resource/local-exec-null/main.tf
+++ Day13-Null-resource/local-exec-null/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # Create the RDS instance
 resource "aws_db_instance" "mysql_rds" {
-  identifier              = "my-mysql-db"
+  identifier              = "my-mysql-db2"
   engine                  = "mysql"
   instance_class          = "db.t3.micro"
   username                = "admin"
@@ -21,10 +21,8 @@ resource "null_resource" "local_sql_exec" {
   depends_on = [aws_db_instance.mysql_rds]
 
   provisioner "local-exec" {
-  command = "mysql --host=${aws_db_instance.mysql_rds.address} --user=admin --password=Password123! dev < init.sql"
-  interpreter = ["bash", "-c"]
-}
-
+    command     = "mysql -h ${aws_db_instance.mysql_rds.address} -u admin -pPassword123! dev < init.sql"
+  }
 
   triggers = {
     always_run = timestamp()
